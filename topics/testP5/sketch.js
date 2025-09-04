d0bacee5584389c44d239e4504ded5c21c02df30
consumedlocusts <samahojabri@gmail.com>
2025-09-04 00:50:56 -0400

    draws worms

--- topics/testP5/sketch.js
+++ topics/testP5/sketch.js
@@ -1,7 +1,14 @@
 function setup() {
   createCanvas(400, 400);
+  // Paint the background once.
+  background("brown")
+  describe('A green circle on a brown background. The circle follows the mouse as the user moves, leaving a trail. The circle changes color to green when the user double clicks.');
 }
 
 function draw() {
-  background(aqua);
+
+  // Draw circles repeatedly.
+  circle(mouseX,mouseY,40)
 }
+
+

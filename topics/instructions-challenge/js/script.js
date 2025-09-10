ad92e690bcffdcc471783e9d658015c05e6906a7
consumedlocusts <samahojabri@gmail.com>
2025-09-10 18:35:29 -0400

    idk

---
+++ topics/instructions-challenge/js/script.js
@@ -0,0 +1,27 @@
+
+"use strict";
+
+/**
+ *  Creates the canvas
+ */
+function setup() {
+    createCanvas (800, 400)
+    background(0,45,0)
+}
+
+"use strict";
+
+/**
+ * sets background, draws the eye
+*/
+function draw() {
+//the void
+background("#00000")
+Fill(255,0,0);
+ellipse(20,20,20,20);
+
+// the eye
+drawEye();
+
+
+}

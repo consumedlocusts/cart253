90ea9c9dd6bd2f03453c740787f08d4814a985d2
consumedlocusts <samahojabri@gmail.com>
2025-09-10 20:46:09 -0400

    j

--- topics/untitled folder/sketch.js
+++ topics/untitled folder/sketch.js
@@ -1,20 +1,319 @@
-
+"use strict";
 /**
  *  Creates the canvas
  */
 function setup() {
-    createCanvas (800, 400)
-    background(0,45,0)
+    createCanvas (800,600);
 }
-
-"use strict";
 
 /**
- * sets background, draws the eye
+ * sets background, draws all content 
 */
 function draw() {
+    background("#e9dcb4ff");
+     
+    push();
+    fill("#000000ff");
+   noStroke();
+   triangle(200,300,300,200,8,100)
+    pop();
 
-    
+     push();
+    fill("#00000071");
+   noStroke();
+   triangle(200,200,200,10,800,600)
+    pop();
 
+   push();
+    fill("#000000ff");
+   noStroke();
+    rect(400,50,20,800);
+    pop();
 
+    push();
+    fill("#000000ff");
+    noStroke();
+    circle(410,170,95);
+    pop();
+
+    push();
+    fill("#ffffffff");
+    stroke("#000000ff");
+    strokeWeight(20);
+    rect(200,400,200,25);
+    pop();
+push();
+    fill("#bb1a1aff");
+   noStroke();
+    rect(400,50,20,800);
+    pop();
+
+    push();
+    fill("#bb1a1aff");
+    noStroke();
+    circle(410,170,95);
+    pop();
+
+    push();
+    fill("#ffffffff");
+    stroke("#000000ff");
+    strokeWeight(20);
+    rect(200,400,200,25);
+    pop();
+
+    push();
+    fill("#ffffffff");
+    stroke("#000000ff");
+    strokeWeight(40);
+    rect(300,200,300,200);
+    pop();
+
+    push();
+    fill("#d4d4d4ff");
+    stroke("#000000ff");
+    strokeWeight(20);
+    rect(300,400,300,200);
+    pop();
+
+    push();
+    fill("#ffffffff");
+    stroke("#000000ff");
+    strokeWeight(20);
+    rect(200,200,300,200);
+    pop();
+  
+    push();
+    fill("#ffffffff");
+    stroke("#000000ff");
+    strokeWeight(10);
+    rect(200,500,300,200);
+    pop();
+  push();
+    fill("#ffffffff");
+    stroke("#000000ff");
+    strokeWeight(20);
+    rect(200,300,30,20);
+    pop();
+      push();
+    fill("#ffffffff");
+    stroke("#000000ff");
+    strokeWeight(20);
+    rect(300,300,30,20);
+    pop();
+       push();
+    fill("#ffffffff");
+    stroke("#000000ff");
+    strokeWeight(20);
+    rect(400,300,30,20);
+    pop();
+       push();
+    fill("#ffffffff");
+    stroke("#000000ff");
+    strokeWeight(20);
+    rect(450,300,30,20);
+    pop();
+
+       push();
+    fill("#ffffffff");
+    stroke("#cb0e0eff");
+    strokeWeight(20);
+    rect(300,300,20,10);
+    pop();
+      push();
+    fill("#ffffffff");
+    stroke("#cb0e0eff");
+    strokeWeight(20);
+    rect(200,300,20,10);
+    pop();
+      push();
+    fill("#ffffffff");
+    stroke("#cb0e0eff");
+    strokeWeight(20);
+    rect(400,300,20,10);
+    pop();
+     push();
+    fill("#ffffffff");
+    stroke("#cb0e0eff");
+    strokeWeight(20);
+    rect(400,300,20,10);
+    pop();
+     push();
+    fill("#ffffffff");
+    stroke("#cb0e0eff");
+    strokeWeight(20);
+    rect(200,800,20,10);
+    pop();
+
+     push();
+    fill("#ffffffff");
+    stroke("#c56e34ff");
+    strokeWeight(20);
+    rect(100,600,800,10);
+    pop();
+
+     push();
+    fill("#ffffffff");
+    stroke("#cb0e0eff");
+    strokeWeight(20);
+    rect(400,600,200,10);
+    pop();
+     push();
+    fill("#718e64ff");
+    stroke("#cb0e0eff");
+    strokeWeight(20);
+    rect(600,400,200,200);
+    pop();
+     
+    push();
+    fill("#ebe79cff");
+    stroke("#cb0e0eff");
+    strokeWeight(20);
+    rect(600,400,20,20);
+    pop();
+
+    push();
+    fill("rgba(79, 89, 35, 1)");
+    stroke("#cb0e0eff");
+    strokeWeight(20);
+    rect(600,500,20,20);
+    pop();
+
+    push();
+    fill("#f50a0aff");
+    stroke("#c00000ff");
+    strokeWeight(20);
+    circle(100,10,250);
+    pop();
+
+    push();
+    fill("#81a232ff");
+    stroke("#256335ff");
+    strokeWeight(10);
+    rect(600,400,20,20);
+    pop();
+
+    push();
+    fill("#81a232ff");
+    stroke("#256335ff");
+    strokeWeight(10);
+    rect(600,500,20,20);
+    pop();
+
+    push();
+    fill("#81a232ff");
+    stroke("#256335ff");
+    strokeWeight(10);
+    rect(600,450,20,20);
+    pop();
+    push();
+    fill("#81a232ff");
+    stroke("#256335ff");
+    strokeWeight(10);
+    rect(600,550,20,20);
+    pop();
+    push();
+    fill("#81a232ff");
+    stroke("#256335ff");
+    strokeWeight(10);
+    rect(600,590,20,20);
+    pop();
+
+    push();
+    fill("#81a232ff");
+    stroke("#256335ff");
+    strokeWeight(10);
+    rect(600,400,20,20);
+    pop();
+
+    push();
+    fill("#81a232ff");
+    stroke("#256335ff");
+    strokeWeight(10);
+    rect(600,500,20,20);
+    pop();
+push();
+    fill("#81a232ff");
+    stroke("#256335ff");
+    strokeWeight(10);
+    rect(700,400,20,20);
+    pop();
+
+    push();
+    fill("#81a232ff");
+    stroke("#256335ff");
+    strokeWeight(10);
+    rect(700,500,20,20);
+    pop();
+    push();
+    fill("#81a232ff");
+    stroke("#256335ff");
+    strokeWeight(10);
+    rect(700,450,20,20);
+    pop();
+    push();
+    fill("#81a232ff");
+    stroke("#256335ff");
+    strokeWeight(10);
+    rect(700,550,20,20);
+    pop();
+    push();
+    fill("#81a232ff");
+    stroke("#256335ff");
+    strokeWeight(10);
+    rect(700,590,20,20);
+    pop();
+    push();
+    fill("#81a232ff");
+    stroke("#256335ff");
+    strokeWeight(10);
+    rect(650,400,20,20);
+    pop();
+
+    push();
+    fill("#81a232ff");
+    stroke("#256335ff");
+    strokeWeight(10);
+    rect(650,500,20,20);
+    pop();
+
+    push();
+    fill("#81a232ff");
+    stroke("#256335ff");
+    strokeWeight(10);
+    rect(650,450,20,20);
+    pop();
+    push();
+    fill("#81a232ff");
+    stroke("#256335ff");
+    strokeWeight(10);
+    rect(650,550,20,20);
+    pop();
+    push();
+    fill("#81a232ff");
+    stroke("#256335ff");
+    strokeWeight(10);
+    rect(650,590,20,20);
+    pop();
+
+    push();
+    fill("#81a232ff");
+    stroke("#256335ff");
+    strokeWeight(10);
+    rect(650,400,20,20);
+    pop();
+
+    push();
+    fill("#81a232ff");
+    stroke("#256335ff");
+    strokeWeight(10);
+    rect(650,500,20,20);
+    pop();
 }
+
+
+
+
+
+
+
+

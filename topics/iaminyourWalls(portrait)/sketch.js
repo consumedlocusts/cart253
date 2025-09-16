098961825bb97eccc22c4a84e1ed5e86455f320d
consumedlocusts <samahojabri@gmail.com>
2025-09-15 22:52:15 -0400

    noo

---
+++ topics/iaminyourWalls(portrait)/sketch.js
@@ -0,0 +1,41 @@
+
+
+/**
+ * Creates the canvas
+ */
+let string = "¯\_(ツ)_/¯i̴̻̺̿̐͌̈́͌à̸͎͓͚̔͐̕̕m̴̼͉͚̓̈́̾̓̀͝i̸̢̺͑͋̐̕̕͝n̸̼͕͙̽̿͛͑̕ÿ̵͍͙̝́͋͒̈́͊ö̸̙̻̞́̓̒̿͊̒u̴͇̞̫͋̿̕͠r̴͕̝̈́͆͛̔̕͘W̴͎͙͙̐̔̽͒̀́a̴̪̦͙̔͑̽̽̚͠l̵͉͙͖̒̾͌͌͌͆l̵̡͚͖͛̓̒͌̕s̸̢̝̟̔͆͐̓͠͝Δμ∂┏(-_-)┛∑∏​π∫i̴̻̺̿̐͌̈́͌à̸͎͓͚̔͐̕̕m̴̼͉͚̓̈́̾̓̀͝i̸̢̺͑͋̐̕̕͝n̸̼͕͙̽̿͛͑̕ÿ̵͍͙̝́͋͒̈́͊ö̸̙̻̞́̓̒̿͊̒u̴͇̞̫͋̿̕͠r̴͕̝̈́͆͛̔̕͘W̴͎͙͙̐̔̽͒̀́a̴̪̦͙̔͑̽̽̚͠l̵͉͙͖̒̾͌͌͌͆l̵̡͚͖͛̓̒͌̕s̸̢̝̟̔͆͐̓͠͝Ω√≈​ÉΔ¯\_(ツ)_/¯∂∑∏​π∫"
+let font; 
+let value=fill("#c2c2c2b7"); 
+
+function setup() {
+  createCanvas(500, 500);
+  
+  // A one time background fill
+  background("#181819ff");
+  
+}
+
+function draw() {
+  
+  push();
+  if (mouseIsPressed === true) {
+    fill("#c2c2c2b7");
+  } else {
+  const weight = map(abs(movedX), 0, 30, 10, 6);
+  strokeWeight(weight);
+  
+  const d = dist(mouseX, mouseY, width/2, height/2);
+
+  const strokeColor = map(d, -width/2, width/2, 0, 100);
+  stroke(strokeColor);
+
+  }
+  
+  textSize(20);
+  // Draw a line from the previous mouse position to the current one
+  text(string, pmouseX, pmouseY, mouseX, mouseY);
+
+  pop(); 
+  
+}
+
---
+++ topics/sketch.js
@@ -0,0 +1,19 @@
+"use strict";
+
+
+function setup() {
+  createCanvas(400, 400);
+  background("brown");
+
+}
+
+function draw() {
+ push();
+
+fill("#000000ff");
+ circle(10);
+
+  pop();
+ 
+  }
+

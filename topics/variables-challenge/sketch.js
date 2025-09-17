e28bb9b93a96ef7b6d3dc9d1db98461f7b631c4c
consumedlocusts <samahojabri@gmail.com>
2025-09-17 19:21:14 -0400

    hellp

--- topics/variables-challenge/sketch.js
+++ topics/variables-challenge/sketch.js
@@ -1,7 +1,56 @@
+
+"use strict";
+
+
+let scope2 = { 
+
+  x: 200,
+    y:200,
+    size: 200
+}
+
+let scope = {
+  x: 200,
+    y: 200,
+    size: 150
+  }
+
+let pointer = { 
+  x: 200,
+    y:200,
+    size: 50
+  }
+
 function setup() {
   createCanvas(400, 400);
+ background("#000000");
+
 }
 
 function draw() {
-  background(220);
+
+  //scope level 1
+  push();
+  fill("#0000");
+  strokeWeight(40);
+  stroke("#f9f511ff");
+  ellipse(mouseX,mouseY,scope.x,scope.y,scope.size,scope.size);
+pop();
+
+//scope level 2
+
+push();
+fill("#0000");
+strokeWeight(20);
+stroke("#4fb419ff");
+ellipse(mouseX,mouseY,scope2.x,scope2.y, scope2.size,scope2.size);
+pop();
+
+push();
+fill("#0000");
+strokeWeight(2);
+stroke("#f50000ff");
+ellipse(mouseX,mouseY,pointer.x, pointer.y, pointer.size,pointer.size);
+pop();
+
 }

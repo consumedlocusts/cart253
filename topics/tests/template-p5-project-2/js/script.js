fb44d0f74c3d59efefc85f2657ea70e1fe03523f
consumedlocusts <samahojabri@gmail.com>
2025-09-28 19:00:55 -0400

    i have a cold

--- topics/tests/template-p5-project-2/js/script.js
+++ topics/tests/template-p5-project-2/js/script.js
@@ -1,65 +1,78 @@
 /**
- * tired me
+ * tired me2
  * sama
  * 
  * Drawing with altering one text string into another
- * Each string is still joined, but "ungrouped" to be altered per character in the string
+ * Each string is still joined, but "ungrouped" to be altered 
+ * per character in the string(image/pixel variation)
  */
-
+//note: i keep getting columns and rows mixed up in the notes sorry 
 "use strict";
 
-//let font;
-
+let sama;
 let startIndex = 0;
 let mouseOverlapsText = 0;
 let chars =[]; // empty list == group
-//let otherFill = ("#f22424ff")
   let startString = "1234567890123456789012345678901234567890"
   let otherString = "qwertyuioplkjhgfdsazxcvbnmkiuytgvbhgfderf"
 
-
-//function preload() { sy= loadImage('/Users/s_hojabr/Documents/GitHub/cart253/topics/texttemp/assets/istockphoto-614744860-612x612.jpg')
-
+function preload() {sama = loadImage('assets/sama.png');
 //font = loadFont('/assets/SpecialElite-Regular.ttf');
-//}
+}
 
 function setup() { 
   createCanvas(640,640);
   background(0)
-//textFont(font);
-  textSize(30);
-textAlign(LEFT, TOP); //align as one would read/type the text 
+
+sama.loadPixels();
+sama.resize(64,69);//chars per row,column
+
+textSize(10);
+textAlign(LEFT, TOP); //align as one would read/type text 
 
 
 //text in columns and rows to cover the canvas using approx width/height of each character
-let charWid= textWidth ("M"); //after prolonged research on why this wasnt working it was because it needs the widest char of all time not just in my text
-let charHi= textAscent()+textDescent(); // this is the height of each character, it differs per letter 
-let horz = floor(width/charWid); //floor calcs roughly each char & lets columns/rows to not over lap
-let vert = floor(height/charHi);
-let grid = vert*horz
-//console.log(startString[1]) 
+let charWid= textWidth ("M"); //after prolonged research on why this wasnt working it was because
+//it needs the widest char of all time not just in my text for an average
+let charHi= textAscent()+textDescent(); //this is the height of each character, it differs per letter 
+let horz = floor(width/charWid); //floor rounds down to the nearest integer, making the row a whole number because i need array index 
+let vert = floor(height/charHi); //floor also makes it so they dont overlap
+let grid = vert*horz //basic area 
+
 //loop - repeat code inside the {} 
+//this loop is considering the individual details of the above
 for(let counter =0; 
   counter<grid; 
   counter =counter+1)
 {
-  //
+  //each sole column and row but seperated so i can map the pixels of me to them
   let h = counter%horz;           
-  let v = floor(counter/horz); 
+  let v = floor(counter/horz); //hours googling this calc still made no sense 
   console.log(startString[counter]);
+  //how these pixels shall spawn 
+let me = (h * sama.width + v) * 4; //where the first coordinate (at the first row, offset column) to allow
+// my pixels to be arrayed , * 4 because each pixel is made up of 4 components: RGBA (photoshop theory)
+let r = sama.pixels[me];//red pixel channel
+let g = sama.pixels[me + 1];//red+1=green pix chanl
+let b = sama.pixels[me + 2];//red+2=blue pix chanl
+sama.updatePixels();
+//average of the rbg scale to get grey scale, "brightness"index
+//is just whichever char's density is closest to the actual "pixel size"(rgb channel)
+let brightness = (r + g + b) / 3;
+let charPaint = "@#%*+=-:. "; // my ASCII char palette index, 
+// any chars i want will get "greyscaled" into my image(dont forget space/empty)!!
+let charIndex = floor(map(brightness, 0, 255, 0, charPaint.length)); //mapping...
+if (charIndex >= charPaint.length) charIndex = charPaint.length - 1;
+let mappedChar = charPaint[charIndex];
   // add character to list
   chars.push({
     listString: startString[counter%startString.length], //length because of text orientation
       x: h*charWid, 
       y: v*charHi,
-      otherListString: otherString[counter%otherString.length]
+      otherListString: mappedChar //otherString[counter%otherString.length]
     });
   }
-
-
 }
-
-
 function checkInput() {  
     for(let counter =0; 
       counter<chars.length;

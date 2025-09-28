83e93bd81d3752a9c3e279001b4c13691f3a8891
consumedlocusts <samahojabri@gmail.com>
2025-09-28 16:23:44 -0400

    loop

--- topics/tests/texttemp/js/script.js
+++ topics/tests/texttemp/js/script.js
@@ -1,9 +1,9 @@
 /**
- * Dr
- * Author Name
+ * tired me
+ * sama
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Drawing with altering one text string into another
+ * Each string is still joined, but "ungrouped" to be altered per character in the string
  */
 
 "use strict";
@@ -14,10 +14,10 @@ let startIndex = 0;
 let mouseOverlapsText = 0;
 let chars =[]; // empty list == group
 //let otherFill = ("#f22424ff")
-  let startString = "¯\_(ツ)_/¯¯\_(ツ)_/¯"
-  let otherString = "1234567890"
+  let startString = "1234567890123456789012345678901234567890"
+  let otherString = "qwertyuioplkjhgfdsazxcvbnmkiuytgvbhgfderf"
 
-//let distance2 = dist (mouseX,mouseY,test2.x,test2.y);
+
 //function preload() { billy= loadImage('/Users/s_hojabr/Documents/GitHub/cart253/topics/texttemp/assets/istockphoto-614744860-612x612.jpg')
 
 //font = loadFont('/assets/SpecialElite-Regular.ttf');
@@ -27,14 +27,14 @@ function setup() {
   createCanvas(640,640);
   background(0)
 //textFont(font);
-  textSize(36);
-textAlign(LEFT, TOP); //idk might change with wrap
+  textSize(30);
+textAlign(LEFT, TOP); //align as one would read/type the text 
 
 
 //text in columns and rows to cover the canvas using approx width/height of each character
-let charWid= textWidth ("M");
-let charHi= textAscent()+textDescent();
-let horz = floor(width/charWid);
+let charWid= textWidth ("M"); //after prolonged research on why this wasnt working it was because it needs the widest char of all time not just in my text
+let charHi= textAscent()+textDescent(); // this is the height of each character, it differs per letter 
+let horz = floor(width/charWid); //floor calcs roughly each char & lets columns/rows to not over lap
 let vert = floor(height/charHi);
 let grid = vert*horz
 //console.log(startString[1]) 
@@ -49,7 +49,7 @@ for(let counter =0;
   console.log(startString[counter]);
   // add character to list
   chars.push({
-    listString: startString[counter%startString.length], 
+    listString: startString[counter%startString.length], //length because of text orientation
       x: h*charWid, 
       y: v*charHi,
       otherListString: otherString[counter%otherString.length]
@@ -70,9 +70,11 @@ function checkInput() {
      mouseOverlapsText = (distance < 30 );
      // if overlap
      if(mouseOverlapsText){
+      
         console.log(chars[counter].listString)
         // switch to other string
         chars[counter].listString = chars[counter].otherListString
+         
      }
    
 }
@@ -85,13 +87,15 @@ function draw() {
     background (0);
     checkInput()
    
-    fill("#1d9925b7"); 
+    fill("#f22424ff");
     
     for(let counter =0; 
       counter<chars.length; 
       counter =counter+1)
+      
 {
    push();
+   fill("#1d9925b7"); 
     text(chars[counter].listString, chars[counter].x, chars[counter].y);
     pop()
 }

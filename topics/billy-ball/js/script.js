222de27ca0c150d997b24c9baf1192da50d844ac
consumedlocusts <samahojabri@gmail.com>
2025-09-30 23:48:10 -0400

    prettier final for billly

--- topics/billy-ball/js/script.js
+++ topics/billy-ball/js/script.js
@@ -1,137 +1,116 @@
-
 /**
- * im da bomb, ASCII and Array Project
+ * billy the 8ball, ASCII and Array Project
  * sama
- * 
+ *
  * Drawing with altering one text string into another
- * Each string is still joined, but "ungrouped" to be altered 
+ * Each string is still joined, but "ungrouped" to be altered
  * per character in the string(image/pixel variation)
  */
-//note: i keep getting columns and rows mixed up in the notes sorry 
+//note: i keep getting columns and rows mixed up in the notes sorry
 "use strict";
 
 let charWid;
 let charHi;
 let horz;
 let vert;
-//let font;
+
 let billy;
 let startIndex = 0;
 let mouseOverlapsText = 0;
-let chars =[]; // empty list == group
-  let startString = "1234567890123456789012345678901234567890"
-  let otherString; 
+let chars = []; // empty list == group
+let startString = "1234567890123456789012345678901234567890";
+let otherString;
 
 function preload() {
-billy = loadImage('assets/8ball.jpg');
-//font = loadFont('assets/Bomb!.ttf');
+  billy = loadImage("assets/8ball.jpg");
 }
 
-function setup() { 
-  createCanvas(640,640);
-  background(0)
+function setup() {
+  createCanvas(640, 640);
+  background(0);
 
+  //textFont(font);
+  textSize(15);
+  textAlign(LEFT, BOTTOM); //align as one would read/type text
 
-//textFont(font);
-textSize(15);
-textAlign(LEFT, BOTTOM); //align as one would read/type text 
+  //text in columns and rows to cover the canvas using approx width/height of each character
+  charWid = textWidth("W"); //after prolonged research on why this wasnt working it was because
+  //it needs the widest char in the alphabet of all time not just in my text for an average
+  charHi = textAscent() + textDescent(); //this is the height of each character, it differs per letter
+  horz = floor(width / charWid); //floor rounds down to the nearest integer, making the row a whole number because i need array index
+  vert = floor(height / charHi); //floor also makes it so they dont overlap
+  let grid = horz * vert; //basic area
 
+  billy.resize(horz, vert); //chars per row,column
+  billy.loadPixels();
 
-//text in columns and rows to cover the canvas using approx width/height of each character
-charWid= textWidth ("W"); //after prolonged research on why this wasnt working it was because
-//it needs the widest char in the alphabet of all time not just in my text for an average
-charHi= textAscent()+textDescent(); //this is the height of each character, it differs per letter 
-horz = floor(width/charWid); //floor rounds down to the nearest integer, making the row a whole number because i need array index 
-vert = floor(height/charHi); //floor also makes it so they dont overlap
-let grid = horz*vert//basic area 
+  //loop - repeat code inside the {}
+  //this loop is considering the individual details of the above
+  for (let counter = 0; counter < grid; counter = counter + 1) {
+    //each sole column and row but seperated so i can map the pixels of me to them
+    let h = counter % horz;
+    let v = floor(counter / horz);
+    console.log(startString[counter]);
 
-billy.resize(horz,vert);//chars per row,column
-billy.loadPixels();
+    //how these pixels shall spawn
+    let me = (v * billy.width + h) * 4;
+    let r = billy.pixels[me]; //red pixel channel
+    let g = billy.pixels[me + 1]; //red+1=green pix chanl
+    let b = billy.pixels[me + 2]; //red+2=blue pix chanl
 
-//loop - repeat code inside the {} 
-//this loop is considering the individual details of the above
-for(let counter =0; 
-  counter<grid; 
-  counter =counter+1)
-{
-  //each sole column and row but seperated so i can map the pixels of me to them
-  let h = counter%horz;           
-  let v = floor(counter/horz); 
-  console.log(startString[counter]);
+    let brightness = (r + g + b) / 3;
+    const charPaint = "Ñ@#W$9876543210?!abc;:+=-,._  ";
 
-  //how these pixels shall spawn 
-let me = (v * billy.width + h) * 4; 
-let r = billy.pixels[me];//red pixel channel
-let g = billy.pixels[me + 1];//red+1=green pix chanl
-let b = billy.pixels[me + 2];//red+2=blue pix chanl
-
-let brightness = (r + g + b) / 3;
-const charPaint = "Ñ@#W$9876543210?!abc;:+=-,._  "; 
-
-let charIndex = floor(map(brightness, 0, 255, 0, charPaint.length)); 
-let mappedChar = charPaint[charIndex];
-  // add character to list
-  chars.push({
-    listString: startString[counter%startString.length], 
-      x: h*charWid, 
-      y: v*charHi,
-      otherListString: mappedChar 
+    let charIndex = floor(map(brightness, 0, 255, 0, charPaint.length));
+    let mappedChar = charPaint[charIndex];
+    // add character to list
+    chars.push({
+      listString: startString[counter % startString.length],
+      x: h * charWid,
+      y: v * charHi,
+      otherListString: mappedChar,
     });
   }
 }
-function checkInput() {  
-    for(let counter =0; 
-      counter<chars.length;
-      counter =counter+1)
-{
-  // calculate distance for each letter
-    const distance = dist(mouseX,mouseY,chars[counter].x,chars[counter].y); 
-     mouseOverlapsText = (distance < 50 );
-     // if hovering with mouse
-     if(mouseOverlapsText){
-        console.log(chars[counter].listString)
-        // switch to other string
-        chars[counter].listString = chars[counter].otherListString
-        
-     }
-   
-}
-
-}
-
-//draws the text 
-function draw() { 
-    background (0);
-    //image(billy,0,0);
-    checkInput();
-console.log(chars[0].x,chars[0].y)
-    for (let counter = 0; 
-        counter < chars.length; 
-        counter = counter + 1) 
-        {
-         // calculate distance for each letter
-        const distance = dist(mouseX,mouseY,chars[counter].x,chars[counter].y); 
-        let mouseOverlapsTextTwo = (distance < 50 );
-         // if hovering with mouse
-        if(mouseOverlapsTextTwo && mouseIsPressed){
-         fill("#c60606ff"); // red when hovered in the bottom right corner for a scare
-        } 
- 
-        else {
-          fill("rgba(227, 227, 227, 0.74)"); 
-         }
-   
-    text(chars[counter].listString, chars[counter].x+charWid/2, chars[counter].y+charHi/2);
-   
-    
+function checkInput() {
+  for (let counter = 0; counter < chars.length; counter = counter + 1) {
+    // calculate distance for each letter
+    const distance = dist(mouseX, mouseY, chars[counter].x, chars[counter].y);
+    mouseOverlapsText = distance < 50;
+    // if hovering with mouse
+    if (mouseOverlapsText) {
+      console.log(chars[counter].listString);
+      // switch to other string
+      chars[counter].listString = chars[counter].otherListString;
+    }
   }
-  
-   
 }
-   
-    //&& mouseIsPressed
+
+//draws the text
+function draw() {
+  background(0);
+  checkInput();
+  console.log(chars[0].x, chars[0].y);
+  for (let counter = 0; counter < chars.length; counter = counter + 1) {
+    // calculate distance for each letter
+    const distance = dist(mouseX, mouseY, chars[counter].x, chars[counter].y);
+    let mouseOverlapsTextTwo = distance < 50;
+    //hovering with & pressing reveals a red "transparencey" over the drawn "distance" with mouse
+    if (mouseOverlapsTextTwo && mouseIsPressed) {
+      fill("#c60606ff");
+    } else {
+      fill("rgba(227, 227, 227, 0.74)");
+    }
+
+    text(
+      chars[counter].listString,
+      chars[counter].x + charWid / 2,
+      chars[counter].y + charHi / 2
+    );
+  }
+}
 
 //reference youtube: https://www.youtube.com/watch?v=55iwMYv8tGI
 //reference youtube: https://www.youtube.com/watch?v=4IyeLc6J1Uo
-//reference codes: https://editor.p5js.org/codingtrain/sketches/ytK7J7d5j 
+//reference codes: https://editor.p5js.org/codingtrain/sketches/ytK7J7d5j
 //https://editor.p5js.org/codingtrain/sketches/LNBpdYQHP

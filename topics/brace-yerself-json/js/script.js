5fda5e777d5d5571b7316294f16e0b91dd9f41d2
consumedlocusts <samahojabri@gmail.com>
2025-10-01 20:12:44 -0400

    e

--- topics/brace-yerself-json/js/script.js
+++ topics/brace-yerself-json/js/script.js
@@ -1,20 +1,19 @@
 /**
  * brace.yerself.json
  * sama
- * 
- * drawing with text 
+ *
+ * drawing with text
  */
-//I want to have the scope pointer x ray the target on contact, 
+//I want to have the scope pointer x ray the target on contact,
 // have the target move randomly around the frame - contact then release mouse then move i mean**
 //
 "use strict";
 let target = {
   targetSize: 50,
-  targetFill:"#ff0000ff",
-  x:0,
-  y:0
-}
-
+  targetFill: "#ff0000ff",
+  x: 0,
+  y: 0,
+};
 
 let scope2 = {
   x: 200,
@@ -31,99 +30,86 @@ let scope = {
 let pointer = {
   x: 200,
   y: 200,
-  size:4,
+  size: 4,
 };
 
 function setup() {
   createCanvas(640, 640);
   background("#000000");
-  target.x= random(0,width);
-  target.y = random(0,height);
+  target.x = random(0, width);
+  target.y = random(0, height);
 }
-function checkInput(){  
- let targetDistance = dist(mouseX, mouseY, target.x, target.y);
- const mouseIsOverlapping = (targetDistance < target.targetSize / 2);
- const mouseIsMoving = (movedX !== 0 || movedY !== 0);
+function checkInput() {
+  let targetDistance = dist(mouseX, mouseY, target.x, target.y);
+  const mouseIsOverlapping = targetDistance < target.targetSize / 2;
+  const mouseIsMoving = movedX !== 0 || movedY !== 0;
 
- if (mouseIsOverlapping && mouseIsMoving){ 
-  target.x= random(0,width);
-  target.y = random(0,height);
+  if (mouseIsOverlapping && mouseIsMoving) {
+    target.x = random(0, width);
+    target.y = random(0, height);
+  } else {
+    target.targetFill = "#ffdd00ff";
+  }
 }
-  else {
-    target.targetFill = "#ffdd00ff"
-  }
-  }
 
 //sorry I am still working on this and plan to change the rectVar to a "target".
 function draw() {
   background("#000000");
   checkInput();
-push();
-
-fill(target.targetFill)
-  rect(target.x,target.y,target.targetSize)
+  push();
 
   if (mouseIsPressed === true) {
-
     fill("#f9f511ff");
-  strokeWeight(90);
-  stroke("#f9f511ff");
-  ellipse(mouseX, mouseY, scope.x, scope.y, scope.size, scope.size);
-  
-  //scope level 2
+    strokeWeight(90);
+    stroke("#f9f511ff");
+    ellipse(mouseX, mouseY, scope.x, scope.y, scope.size, scope.size);
 
+    //scope level 2
 
-  fill("#4fb419ff");
-  strokeWeight(90);
-  stroke("#4fb419ff");
-  ellipse(mouseX, mouseY, scope2.x, scope2.y, scope2.size, scope2.size);
-    
-  
-  fill("#4fb419ff");
-  strokeWeight(190);
-  stroke("#000000ff");
-  ellipse(mouseX, mouseY,140,20,0,20);
+    fill("#4fb419ff");
+    strokeWeight(90);
+    stroke("#4fb419ff");
+    ellipse(mouseX, mouseY, scope2.x, scope2.y, scope2.size, scope2.size);
 
-  stroke("#000000d3");
-  strokeWeight(80)
-  fill("#ff0000ff");
-  ellipse(mouseX, mouseY, pointer.x, pointer.y, pointer.size, pointer.size);
-   
-  fill("#ffda35ff");
- noStroke();
-  ellipse(mouseX, mouseY, 10, 200, 0, 5);
-fill("#ffda35ff");
- noStroke();
-ellipse(mouseX, mouseY, 200, 10, 5, 0);
+    fill("#4fb419ff");
+    strokeWeight(190);
+    stroke("#000000ff");
+    ellipse(mouseX, mouseY, 140, 20, 0, 20);
 
+    stroke("#000000d3");
+    strokeWeight(80);
+    fill("#ff0000ff");
+    ellipse(mouseX, mouseY, pointer.x, pointer.y, pointer.size, pointer.size);
 
+    fill("#ffda35ff");
+    noStroke();
+    ellipse(mouseX, mouseY, 10, 200, 0, 5);
+    fill("#ffda35ff");
+    noStroke();
+    ellipse(mouseX, mouseY, 200, 10, 5, 0);
+    fill(target.targetFill);
+    rect(target.x, target.y, target.targetSize);
   } else {
+    fill("#f9f511ff");
+    strokeWeight(170);
+    stroke("#f9f511ff");
+    ellipse(mouseX, mouseY, scope.x, scope.y, scope.size, scope.size);
 
+    fill("#4fb419ff");
+    strokeWeight(150);
+    stroke("#4fb419ff");
+    ellipse(mouseX, mouseY, 200, 200, 600, 600);
 
-  fill("#f9f511ff");
-  strokeWeight(170);
-  stroke("#f9f511ff");
-  ellipse(mouseX, mouseY, scope.x, scope.y, scope.size, scope.size);
- 
-  fill("#4fb419ff");
-  strokeWeight(150);
-  stroke("#4fb419ff");
-  ellipse(mouseX, mouseY, 200,200,600,600);
+    stroke("#000000ec");
+    strokeWeight(130);
+    fill("#ff0000ff");
+    ellipse(mouseX, mouseY, 200, 200, 300, 300);
 
- 
-  stroke("#000000ec");
-  strokeWeight(130);
-  fill("#ff0000ff");
-  ellipse(mouseX, mouseY, 200,200,300,300);
+    stroke("#ff0000ff");
+    strokeWeight(5);
+    fill("#000000ff");
+    ellipse(mouseX, mouseY, 200, 200, 0);
 
-  stroke("#ff0000ff");
-  strokeWeight(5);
-  fill("#000000ff");
-  ellipse(mouseX, mouseY,200,200,0);
-  
-  pop();
-
+    pop();
   }
- 
-  
 }
--- topics/randomhogevent/js/script.js
+++ topics/randomhogevent/js/script.js
@@ -1,24 +1,75 @@
 /**
- * Title of Project
- * Author Name
+ * event
+ * Sama Hojabri
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
- */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
-function setup() {
+"use strict";
+//font change
+let font;
+// Current score
+let score = 0;
 
-}
-
+// Is the game over?
+let gameOver = false;
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
-function draw() {
+ * Create the canvas
+ */
+function preload() {
+  font = loadFont("assets/1Punk.ttf");
+}
+function setup() {
+  createCanvas(400, 400);
+}
 
+/**
+ * Update the score and display the UI
+ */
+function draw() {
+  background("#000000ff");
+
+  // Only increase the score if the game is not over
+  if (!gameOver) {
+    // Score increases relatively slowly
+    score += 0.05;
+  }
+  displayUI();
+}
+function mousePressed() {
+  gameOver = true;
+}
+function keyPressed() {
+  gameOver = true;
+}
+
+/**
+ * Show the game over message if needed, and the current score
+ */
+function displayUI() {
+  if (gameOver) {
+    push();
+    textFont(font);
+    textSize(48);
+    textStyle(BOLD);
+    textAlign(CENTER, CENTER);
+    text("You lose!", width / 2, height / 3);
+    pop();
+  }
+  displayScore();
+}
+
+/**
+ * Display the score
+ */
+function displayScore() {
+  push();
+  textSize(48);
+  textStyle(BOLD);
+  textAlign(CENTER, CENTER);
+  text(floor(score), width / 2, height / 2);
+  pop();
 }

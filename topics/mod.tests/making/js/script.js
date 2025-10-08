60feeeee2a08656cc694dea74d2d908d10b039d1
consumedlocusts <samahojabri@gmail.com>
2025-10-08 18:17:38 -0400

    ?

--- topics/mod.tests/making/js/script.js
+++ topics/mod.tests/making/js/script.js
@@ -1,7 +1,7 @@
 /**
  * Title of Project
  * Author Name
- * 
+ *
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
  */
@@ -10,15 +10,12 @@
 
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+ */
 function setup() {
-
+  createCanvas();
 }
-
 
 /**
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
-function draw() {
-
-}
+ */
+function draw() {}

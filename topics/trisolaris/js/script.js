b9c6cc75cb91458d51e2c1b736291b9cc4ee9ca8
consumedlocusts <samahojabri@gmail.com>
2025-10-22 14:22:19 -0400

    trisolaris returns

--- topics/trisolaris/js/script.js
+++ topics/trisolaris/js/script.js
@@ -21,17 +21,22 @@ function draw() {
   // Sky blue
   background("#87ceeb");
 
-  drawSun1(500, 100, 80);
-  drawSun2(350, 180, 200);
-  drawSun3(120, 100, 160);
+  drawSun(500, 100, 80);
+  drawSun(350, 180, 200);
+  drawSun(120, 100, 160);
 }
 
 /**
  * Draws the first sun
  */
 function drawSun(x, y, size) {
+  const minWeight = 20;
+  const maxWeight = 1;
+  let d = dist(mouseX, mouseY, x, y);
+  let weight = map(d, 0, width, minWeight, maxWeight);
+
   push();
-  noStroke();
+  strokeWeight(weight);
   fill("#f99736");
   ellipse(x, y, size);
   pop();

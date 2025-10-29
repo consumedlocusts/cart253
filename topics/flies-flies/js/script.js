4c44a21c9b3f5fec8b6499ff91ebc0027ee887c6
consumedlocusts <samahojabri@gmail.com>
2025-10-29 19:52:43 -0400

    dewd

--- topics/flies-flies/js/script.js
+++ topics/flies-flies/js/script.js
@@ -1,7 +1,7 @@
 /**
  * Flies flies
  * Pippin Barr
- * 
+ *
  * A program for drawing flies on the canvas. The flies are stored
  * in an array. We display them with a for...of loop
  */
@@ -11,50 +11,54 @@
 // Our array of flies (specifically "fly data" really)
 // Each fly has a position and a size
 let flies = [
-    {
-        x: 100,
-        y: 125,
-        size: 10
-    },
-    {
-        x: 160,
-        y: 170,
-        size: 14
-    },
-    {
-        x: 180,
-        y: 50,
-        size: 5
-    }
+  {
+    x: 100,
+    y: 125,
+    size: 10,
+  },
+  {
+    x: 160,
+    y: 170,
+    size: 14,
+  },
+  {
+    x: 180,
+    y: 50,
+    size: 5,
+  },
 ];
 
 /**
  * Create the canvas
  */
 function setup() {
-    // Let's make it tiny for some reason
-    createCanvas(200, 200);
+  // Let's make it tiny for some reason
+  createCanvas(200, 200);
 }
 
 /**
  * Display the flies in the array
  */
 function draw() {
-    background("#87ceed");
+  background("#87ceed");
 
-    // Display each fly in the array
-    for (let fly of flies) {
-        drawFly(fly);
-    }
+  // Display each fly in the array
+  for (let fly of flies) {
+    drawFly(fly);
+  }
+}
+function moveFly(fly) {
+  fly.x += random(-fly.buzziness, fly.buzziness);
+  fly.y += random(-fly.buzziness, fly.buzziness);
 }
 
 /**
  * Draws the provided fly to the canvas
  */
 function drawFly(fly) {
-    push();
-    noStroke();
-    fill(0);
-    ellipse(fly.x, fly.y, fly.size);
-    pop();
+  push();
+  noStroke();
+  fill(0);
+  ellipse(fly.x, fly.y, fly.size);
+  pop();
 }

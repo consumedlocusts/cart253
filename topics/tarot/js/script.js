d7db5aff9ef37bc629750e7d946732295d5ec1fb
consumedlocusts <samahojabri@gmail.com>
2025-11-13 14:20:06 -0500

    tarots

--- topics/tarot/js/script.js
+++ topics/tarot/js/script.js
@@ -6,11 +6,17 @@
  */
 
 "use strict";
+//tarot file
+let tarot = undefined;
+function preload(){
+    tarot =loadJSON("assets/tarot_interpretations.json")
+}
 
 /**
  * tbd.
 */
 function setup() {
+    createCanvas(800,400);
 
 }
 
@@ -19,5 +25,11 @@ function setup() {
  * tbd.
 */
 function draw() {
+background(0);
+const fool= tarot.tarot_interpretations[0].meanings.shadow[0];
+push()
+textSize(16);
+fill("yellow");
+text(fool,width/2,height/2 );
 
 }

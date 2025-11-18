774566e7cab948706b3ef1c9710de933bd3ac4f5
consumedlocusts <samahojabri@gmail.com>
2025-11-18 03:09:01 -0500

    more html editing

--- topics/locust.host/js/end.js
+++ topics/locust.host/js/end.js
@@ -5,3 +5,23 @@
  * storyline 3 set up for the locust storlines
  */
 
+function endSetup() {
+
+}
+
+/**
+ * This will be called every frame when the blue variation is active
+ */
+function endDraw() {
+    background("blue");
+}
+
+/**
+ * This will be called whenever a key is pressed while the blue variation is active
+ */
+function endMousePressed(event) {
+    if (event.keyCode === 27) {
+        state = "menu";
+    }
+}
+
